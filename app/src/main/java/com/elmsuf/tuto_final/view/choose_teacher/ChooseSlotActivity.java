00c51c1fe04646ec7014ca68c9cfc0c165e2e830
elm-suf <elm.sufian@gmail.com>
2018-12-24 14:15:59 +0100

    created choose_slot_activity

---
+++ app/src/main/java/com/elmsuf/tuto_final/view/choose_teacher/ChooseSlotActivity.java
@@ -0,0 +1,15 @@
+package com.elmsuf.tuto_final.view.choose_teacher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.elmsuf.tuto_final.R;
+
+public class ChooseSlotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_slot);
+    }
+}
